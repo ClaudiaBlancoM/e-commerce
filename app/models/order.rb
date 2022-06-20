68cae9b2719c2bb4f899012388ad1b91f2ab9610
ClaudiaBlancoM <cabm0626@gmail.com>
2022-06-20 00:44:51 -0400

    feat: test order red ok

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,11 @@
 class Order < ApplicationRecord
-  belongs_to :user
+  before_create -> { generate_number(hash_size) }
+
+    def generate_number(size)
+    self.number ||= loop do
+      random = random_candidate(size)
+      break random unless self.class.exists?(number: random)
+    end
+  end
 end
+  
