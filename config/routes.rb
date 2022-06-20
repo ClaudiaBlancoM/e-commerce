c4a6edfc449af7acd201cf85779e37f1fed1ae95
ClaudiaBlancoM <cabm0626@gmail.com>
2022-06-19 22:36:50 -0400

    Devise initializer config with turbo_stream

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :admins
   get 'home/index'
   resources :products
   resources :categories
   root to: "home#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
